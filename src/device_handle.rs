a1ien/rusb

commit ea85b7f9a3bd2a5a1f2f355727678aa504124afd

Removed unsafe buf_slice from read_string_descriptor_ascii() (#18)

src/device_handle.rs
@@ -547,10 +547,8 @@ impl<T: UsbContext> DeviceHandle<T> {
     pub fn read_string_descriptor_ascii(&self, index: u8) -> crate::Result<String> {
         let mut buf = Vec::<u8>::with_capacity(255);
 
-        let buf_slice = unsafe { slice::from_raw_parts_mut(buf.as_mut_ptr(), buf.capacity()) };
-
-        let ptr = buf_slice.as_mut_ptr() as *mut c_uchar;
-        let len = buf_slice.len() as i32;
+        let ptr = buf.as_mut_ptr() as *mut c_uchar;
+        let len = buf.len() as i32;
 
         let res =
             unsafe { libusb_get_string_descriptor_ascii(self.handle.as_ptr(), index, ptr, len) };

commit ad001544122a5d42b281319f3ece0d266b393a71

Fix read_string_descriptor_ascii
After broke in #18

src/device_handle.rs
@@ -547,10 +547,11 @@ impl<T: UsbContext> DeviceHandle<T> {
         let mut buf = Vec::<u8>::with_capacity(255);
 
         let ptr = buf.as_mut_ptr() as *mut c_uchar;
-        let len = buf.len() as i32;
+        let capacity = buf.capacity() as i32;
 
-        let res =
-            unsafe { libusb_get_string_descriptor_ascii(self.handle.as_ptr(), index, ptr, len) };
+        let res = unsafe {
+            libusb_get_string_descriptor_ascii(self.handle.as_ptr(), index, ptr, capacity)
+        };
 
         if res < 0 {
             return Err(error::from_libusb(res));