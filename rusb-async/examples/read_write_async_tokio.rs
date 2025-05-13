57d413c4f38eb1c43a7383bf119466ff973e2adf
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-05-13 04:11:08 +0300

    Fix compilation failures of tokio examples on Windows

--- rusb-async/examples/read_write_async_tokio.rs
+++ rusb-async/examples/read_write_async_tokio.rs
@@ -1,81 +1,84 @@
-use libc::{POLLIN, POLLOUT};
-use rusb::{Context, UsbContext};
-use rusb_async::fd::{FdCallbacks, FdEventHandler};
-use rusb_async::transfer::BulkTransfer;
-use tokio::io::unix::AsyncFd;
-use tokio::io::Interest;
-use tokio::task::{JoinHandle, JoinSet};
-
-use std::collections::BTreeMap;
-use std::os::fd::RawFd;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
-
 #[cfg(not(unix))]
-compile_error!("File descriptor event loop integrations are only supported on UNIX-like systems");
-
-struct TokioFdCallbacks {
-    context: Context,
-    fd_handle_map: Mutex<BTreeMap<RawFd, JoinHandle<()>>>,
+fn main() {
+    println!("File descriptor event loop integrations are only supported on UNIX-like systems");
 }
 
-impl TokioFdCallbacks {
-    pub fn new(context: Context) -> Self {
-        Self {
-            context,
-            fd_handle_map: Mutex::new(BTreeMap::new()),
-        }
-    }
-}
-
-impl FdCallbacks for TokioFdCallbacks {
-    type Context = Context;
-
-    fn context(&self) -> &Self::Context {
-        &self.context
-    }
-
-    fn fd_added(&self, fd: RawFd, events: libc::c_short) {
-        let mut interest: Option<Interest> = None;
-
-        if events & POLLIN != 0 {
-            interest = Some(Interest::READABLE);
-        }
-
-        if events & POLLOUT != 0 {
-            interest = interest
-                .map(|i| i.add(Interest::WRITABLE))
-                .or(Some(Interest::WRITABLE));
-        }
-
-        let Some(interest) = interest else {
-            return;
-        };
-
-        let async_fd = AsyncFd::with_interest(fd, interest).unwrap();
-
-        let context = self.context.clone();
-
-        let handle = tokio::spawn(async move {
-            loop {
-                let mut guard = async_fd.ready(interest).await.unwrap();
-                context.handle_events(Some(Duration::ZERO)).unwrap();
-                guard.clear_ready();
-            }
-        });
-
-        self.fd_handle_map.lock().unwrap().insert(fd, handle);
-    }
-
-    fn fd_removed(&self, fd: RawFd) {
-        if let Some(handle) = self.fd_handle_map.lock().unwrap().remove(&fd) {
-            handle.abort();
-        }
-    }
-}
-
+#[cfg(unix)]
 #[tokio::main]
 async fn main() {
+    use libc::{POLLIN, POLLOUT};
+    use rusb::{Context, UsbContext};
+    use rusb_async::fd::{FdCallbacks, FdEventHandler};
+    use rusb_async::transfer::BulkTransfer;
+    use tokio::io::unix::AsyncFd;
+    use tokio::io::Interest;
+    use tokio::task::{JoinHandle, JoinSet};
+
+    use std::collections::BTreeMap;
+    use std::os::fd::RawFd;
+    use std::sync::{Arc, Mutex};
+    use std::time::Duration;
+
+    struct TokioFdCallbacks {
+        context: Context,
+        fd_handle_map: Mutex<BTreeMap<RawFd, JoinHandle<()>>>,
+    }
+
+    impl TokioFdCallbacks {
+        pub fn new(context: Context) -> Self {
+            Self {
+                context,
+                fd_handle_map: Mutex::new(BTreeMap::new()),
+            }
+        }
+    }
+
+    impl FdCallbacks for TokioFdCallbacks {
+        type Context = Context;
+
+        fn context(&self) -> &Self::Context {
+            &self.context
+        }
+
+        fn fd_added(&self, fd: RawFd, events: libc::c_short) {
+            let mut interest: Option<Interest> = None;
+
+            if events & POLLIN != 0 {
+                interest = Some(Interest::READABLE);
+            }
+
+            if events & POLLOUT != 0 {
+                interest = interest
+                    .map(|i| i.add(Interest::WRITABLE))
+                    .or(Some(Interest::WRITABLE));
+            }
+
+            let Some(interest) = interest else {
+                return;
+            };
+
+            let async_fd = AsyncFd::with_interest(fd, interest).unwrap();
+
+            let context = self.context.clone();
+
+            let handle = tokio::spawn(async move {
+                loop {
+                    let mut guard = async_fd.ready(interest).await.unwrap();
+                    context.handle_events(Some(Duration::ZERO)).unwrap();
+                    guard.clear_ready();
+                }
+            });
+
+            self.fd_handle_map.lock().unwrap().insert(fd, handle);
+        }
+
+        fn fd_removed(&self, fd: RawFd) {
+            if let Some(handle) = self.fd_handle_map.lock().unwrap().remove(&fd) {
+                handle.abort();
+            }
+        }
+    }
+
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 5 {
