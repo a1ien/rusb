d37b3aea42604e8cd13c8fdb635a61a8145ce40f
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-05-14 20:06:56 +0300

    Fix imports for Windows

--- rusb-async/src/lib.rs
+++ rusb-async/src/lib.rs
@@ -2,11 +2,10 @@ mod context;
 mod error;
 mod transfer;
 
+#[cfg(unix)]
+pub use crate::context::{FdCallbacks, FdCallbacksEventHandler, FdEvents};
 pub use crate::{
-    context::{
-        AsyncContext, AsyncUsbContext, EventHandler, EventHandlerData, FdCallbacks,
-        FdCallbacksEventHandler, FdEvents,
-    },
+    context::{AsyncContext, AsyncUsbContext, EventHandler, EventHandlerData},
     error::{Error, Result},
     transfer::{
         BulkTransfer, ControlTransfer, InterruptTransfer, IsoBufIter, IsochronousBuffer,
